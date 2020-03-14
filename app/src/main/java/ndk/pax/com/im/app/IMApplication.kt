9fb7e6d46fe34b2f061c9fcb372f72228bb274ca
Rowen <617490820@qq.com>
2020-03-14 22:41:52 +0800

    Bmob提交

--- app/src/main/java/ndk/pax/com/im/app/IMApplication.kt
+++ app/src/main/java/ndk/pax/com/im/app/IMApplication.kt
@@ -1,6 +1,7 @@
 package ndk.pax.com.im.app
 
 import android.app.Application
+import cn.bmob.v3.Bmob
 import com.hyphenate.chat.EMClient
 import com.hyphenate.chat.EMOptions
 import ndk.pax.com.im.BuildConfig
@@ -16,18 +17,20 @@ class IMApplication :Application(){
 
     override fun onCreate() {
         super.onCreate()
-//        EMOptions options = new EMOptions();
-//// 默认添加好友时，是不需要验证的，改成需要验证
-//        options.setAcceptInvitationAlways(false);
-// 是否自动将消息附件上传到环信服务器，默认为True是使用环信服务器上传下载，如果设为 false，需要开发者自己处理附件消息的上传和下载
-       // options.setAutoTransferMessageAttachments(true);
-// 是否自动下载附件类消息的缩略图等，默认为 true 这里和上边这个参数相关联
-       // options.setAutoDownloadThumbnail(true);
-//初始化
+        // EMOptions options = new EMOptions();
+        //// 默认添加好友时，是不需要验证的，改成需要验证
+        // options.setAcceptInvitationAlways(false);
+        // 是否自动将消息附件上传到环信服务器，默认为True是使用环信服务器上传下载，如果设为 false，需要开发者自己处理附件消息的上传和下载
+        // options.setAutoTransferMessageAttachments(true);
+        // 是否自动下载附件类消息的缩略图等，默认为 true 这里和上边这个参数相关联
+        // options.setAutoDownloadThumbnail(true);
+        //初始化
         EMClient.getInstance().init(applicationContext, EMOptions());
-//在做打包混淆时，关闭debug模式，避免消耗不必要的资源
+        //在做打包混淆时，关闭debug模式，避免消耗不必要的资源
         EMClient.getInstance().setDebugMode(BuildConfig.DEBUG);
 
+        //Bmob
+        Bmob.initialize(applicationContext,"2ee4708569a21a8922b2020ab5a365b7");
     }
 
 }
