ac9aee3cb16d0569d3a85727a0c5cccfd4357a40
Rowen <617490820@qq.com>
2020-03-15 13:47:52 +0800

    动态界面退出登录

--- app/src/main/java/ndk/pax/com/im/ui/fragment/DynamicFragment.kt
+++ app/src/main/java/ndk/pax/com/im/ui/fragment/DynamicFragment.kt
@@ -5,6 +5,14 @@ import kotlinx.android.synthetic.main.fragment_dynamic.*
 import kotlinx.android.synthetic.main.header.*
 import ndk.pax.com.im.R
 import kotlin.math.log
+import com.hyphenate.EMCallBack
+import ndk.pax.com.im.adapter.EMCallBackAdapter
+import ndk.pax.com.im.ui.activity.LoginActivity
+import ndk.pax.com.im.ui.activity.MainActivity
+import org.jetbrains.anko.runOnUiThread
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
+
 
 /**
  * User：Rowen
@@ -23,6 +31,34 @@ class DynamicFragment:BaseFragment(){
         val logoutString=String.format(getString(R.string.logout), EMClient.getInstance().currentUser)
         logout.text= logoutString
 
+        //退出登录监听
+        logout.setOnClickListener {
+            logout();
+        }
+    }
+
+    fun logout(){
+        EMClient.getInstance().logout(true, object : EMCallBackAdapter() {
+            override fun onSuccess() {
+                context?.runOnUiThread {
+                    context?.toast(R.string.logout_success)
+                    context?.startActivity<LoginActivity>()
+                    activity?.finish();
+                }
+            }
+            override fun onError(p0: Int, p1: String?) {
+                //切换主线程
+                context?.runOnUiThread {
+                    toast(R.string.logout_failed)
+                }
+            }
+        })
 
     }
+
+
+
+
+
+
 }
